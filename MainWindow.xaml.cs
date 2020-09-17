d88b84439eef47520dbaa4a934c2fb64aa64eced
makeappx <topodthegamepc@gmail.com>
2020-09-17 23:14:17 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SignExProj
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Process process;
+        public MainWindow()
+        {
+            InitializeComponent();
+            System.Timers.Timer t = new System.Timers.Timer(2000);
+            t.Elapsed += (object _, ElapsedEventArgs __) =>
+            {
+                Dispatcher.Invoke(() => 
+                ProcessesList.ItemsSource = 
+                Process.GetProcesses().Select(x => (x.ProcessName, x.Id)).OrderBy(x => x.ProcessName));
+            };
+            t.Enabled = true;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CheckLength()) return;
+            byte[] bytes_scan;
+            byte[] bytes_write;
+            if (BaseBox.SelectedIndex == 0)
+                try
+                {
+                    bytes_scan = Text1.Text.Split(' ').Where(x => x != string.Empty).Select(byte.Parse).ToArray();
+                    bytes_write = Text2.Text.Split(' ').Where(x => x != string.Empty).Select(byte.Parse).ToArray();
+                }
+                catch { MessageBox.Show("Some values can't be parsed","Error"); return; }
+            else
+            {
+                try
+                {
+                    bytes_scan = Text1.Text.Split(' ').Where(x => x != string.Empty).
+                        Select(x => byte.Parse(x, System.Globalization.NumberStyles.HexNumber)).ToArray();
+                    bytes_write = Text2.Text.Split(' ').Where(x => x != string.Empty).
+                        Select(x => byte.Parse(x, System.Globalization.NumberStyles.HexNumber)).ToArray();
+                }
+                catch { MessageBox.Show("Some values can't be parsed", "Error"); return; }
+            }
+            SignEX s = new SignEX(process);
+            if (s.WriteBytes(bytes_scan, bytes_write, true))
+                MessageBox.Show("Bytes were succesfully injected", "OK");
+            else MessageBox.Show("Error");
+        }
+        private bool CheckLength()
+        {
+            int a = Text1.Text.Split(new [] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            int b = Text2.Text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            if (a > b)
+            {
+                if (MessageBox.Show("Bytes lengths should be equal\nDo you want to add nops?",
+                                "Warning", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                    for (int i = 0; i < a - b; i++)
+                        if (BaseBox.SelectedIndex == 0) Text2.Text += " 144";
+                        else Text2.Text += " 90";
+            }
+            else if (a < b)
+            {
+                if (MessageBox.Show("Bytes for scan can't be less than bytes for write",
+                                "Error", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+                    return false;
+            }
+            return true;
+        }
+
+        private void RefreshP(object sender, MouseButtonEventArgs e)
+        {
+         
+        }
+        private void CloseP(object sender, MouseButtonEventArgs e) =>
+            process.Close();
+        private void KillP(object sender, MouseButtonEventArgs e) =>
+            process.Kill();
+        private void ProcessesList_SelectionChanged(object sender, SelectionChangedEventArgs e) =>
+            process = Process.GetProcessById((((string, int))ProcessesList.SelectedItem).Item2);
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            string s = Text1.Text;
+            Text1.Text = Text2.Text;
+            Text2.Text = s;
+        }
+    }
+}
---
+++ SignEX.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+public class SignEX
+{
+	IntPtr handle;
+	public SignEX(string ProcessName) { handle = GetHandle(ProcessName); }
+	public SignEX(int pID) { handle = OpenProcess(2035711, false, pID); }
+	public SignEX(IntPtr pHandle) { handle = pHandle; }
+    public SignEX(Process process) { handle = process.Handle; }
+	public struct MEMORY_BASIC_INFORMATION
+	{
+		public IntPtr BaseAddress;
+		public IntPtr AllocationBase;
+		public uint AllocationProtect;
+		public IntPtr RegionSize;
+		public uint State;
+		public uint Protect;
+		public uint Type;
+	}
+	private IntPtr GetHandle(string name) 
+    {
+		Process[] p = Process.GetProcessesByName(name);
+		return p.Length > 0 ? p[0].Handle : (IntPtr)(-1);
+    }
+    private int SignScan(byte[] bytes_scan) 
+    {
+        MEMORY_BASIC_INFORMATION zero = new MEMORY_BASIC_INFORMATION();
+        int baseAddress = 0, num = 0;
+		byte[] numArray;
+		while (baseAddress <= Int32.MaxValue)
+        {
+            VirtualQueryEx(handle, (IntPtr)baseAddress, out zero, (uint)Marshal.SizeOf(typeof(MEMORY_BASIC_INFORMATION)));
+			if (baseAddress != (int)zero.BaseAddress + (int)zero.RegionSize)
+			{
+				baseAddress = (int)zero.BaseAddress + (int)zero.RegionSize;
+				try
+				{
+					numArray = new byte[(int)zero.RegionSize];
+				}
+				catch { return -1; }
+				ReadProcessMemory(handle, zero.BaseAddress, numArray, (int)zero.RegionSize, out _);
+				int x = -1;
+				while (++x < (int)zero.RegionSize)
+					if (numArray[x] == bytes_scan[0])
+						for (int i = 0; i < (int)bytes_scan.Length; i++)
+							if (numArray[i + x] != bytes_scan[i])
+							{
+								num = 0;
+								break;
+							}
+							else
+							{
+								num++;
+								if (num == (int)bytes_scan.Length)
+									return (int)zero.BaseAddress + x;
+							}
+			}
+			else break;
+        }
+        return -1;
+    }
+
+	/// <summary>
+	/// Returns True or False whether code was injected or not
+	/// </summary>
+	/// <param name="bytes_scan"></param>
+	/// <param name="bytes_write"></param>
+	/// <returns></returns>
+	public bool WriteBytes(byte[] bytes_scan, byte[] bytes_write, bool forAll) 
+    {
+		int num = SignScan(bytes_scan);
+        if (num != -1)
+			return WriteProcessMemory(handle, (IntPtr)num, bytes_write, (uint)bytes_write.Length, out _);
+		return false;
+    }
+	
+	#region kernel32
+	[DllImport("kernel32.dll", CharSet = CharSet.None, ExactSpelling = false, SetLastError = true)]
+    private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UIntPtr lpNumberOfBytesWritten);
+	[DllImport("kernel32.dll", CharSet = CharSet.None, ExactSpelling = false)]
+	public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+	[DllImport("kernel32.dll", CharSet = CharSet.None, ExactSpelling = false, SetLastError = true)]
+	private static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out int lpNumberOfBytesRead);
+	[DllImport("kernel32.dll", CharSet = CharSet.None, ExactSpelling = false, SetLastError = true)]
+    private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, IntPtr lpBuffer, uint nSize, UIntPtr lpNumberOfBytesWritten);
+    private void WriteProcessMemory(IntPtr handle, IntPtr addr, object bytes_write, uint length, out UIntPtr dammy)
+    {
+        throw new NotImplementedException();
+    }
+	[DllImport("kernel32.dll", CharSet = CharSet.None, ExactSpelling = false)]
+	private static extern int VirtualQueryEx(IntPtr hProcess, IntPtr lpAddress, out MEMORY_BASIC_INFORMATION lpBuffer, uint dwLength);
+	#endregion
+}
